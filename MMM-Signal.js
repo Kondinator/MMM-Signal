7dc9099340e2d596854ff711557f183e5523f643
Kondinator <Kondiersej@live.dk>
2019-03-08 12:55:40 +0100

    first try babee

--- MMM-Signal.js
+++ MMM-Signal.js
@@ -12,7 +12,26 @@ Module.register("MMM-Signal", {
         signalLys.style.height = '100%';
         document.body.appendChild(signalLys);
 
-        var tempsignalfarve;
+        var tempsignalfarve = 'blue'
+        var senesteTempSkift;
+
+
+        function addZero(i) {
+            if (i < 10) {
+                i = "0" + i;
+            }
+            return i;
+        }
+
+        function logTime() {
+            var now = new Date();
+            var m = addZero(now.getMinutes());
+            var h = addZero(now.getHours());
+            var t = "" + h + ":" + m;
+            return senesteTempSkift = String(t);
+
+
+        }
 
         function loadVandTemp() {
 
@@ -26,23 +45,39 @@ Module.register("MMM-Signal", {
                     vandTempTal = xhttp.responseText; //sæt Math.round på for at kun vise Runde tal
                     console.log('vandtemp = ' + vandTempTal)
 
-                    if (vandTempTal < 30) {
+                    if (vandTempTal < 30 && vandTempTal > 20.1) {
+                        if (tempsignalfarve !== 'green') {
+                            console.log('changed to green')
+                            logTime()
+
+                        }
                         tempsignalfarve = 'green'
+
                     }
 
-                    if (vandTempTal < 20) {
+                    if (vandTempTal < 20 && vandTempTal > 10.1) {
+                        if (tempsignalfarve !== 'yellow') {
+                            console.log('changed to yellow')
+                            logTime()
+                        }
                         tempsignalfarve = 'yellow'
                     }
 
-                    if (vandTempTal < 10) {
+                    if (vandTempTal < 10 && vandTempTal > 0) {
+                        if (tempsignalfarve !== 'red') {
+                            console.log('changed to red')
+                            logTime()
+                        }
                         tempsignalfarve = 'red'
                     }
+
 
                 }
 
             };
             xhttp.open("GET", "http://10.10.10.191", false);
             xhttp.send();
+
             console.log(tempsignalfarve)
 
         }
@@ -57,6 +92,10 @@ Module.register("MMM-Signal", {
                 res.arc(100, 75, 50, 0, 2 * Math.PI);
                 res.fillStyle = tempsignalfarve;
                 res.fill();
+                res.fillStyle = "red";
+                res.textAlign = "center";
+                res.font = "30px Arial";
+                res.fillText(senesteTempSkift, canvas.width / 2, canvas.height / 2);
             }
             draw()
         }
